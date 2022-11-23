226a10ec07abd9029288f53231199a6599687a65
Fernanda Gonçalves Barros <69534090+nandagb@users.noreply.github.com>
2022-11-23 20:44:15 -0300

    teste para testar a funcao de combinacao

--- app/src/main/java/problema/carteiro/chines/Main.java
+++ app/src/main/java/problema/carteiro/chines/Main.java
@@ -200,6 +200,39 @@ public class Main {
         
     }
 
+    public static void Teste() {
+        Algoritmos main = new Algoritmos();
+
+        Grafo grafoNaoEuleriano = new Grafo();
+
+        grafoNaoEuleriano.addVertice(1);
+        grafoNaoEuleriano.addVertice(2);
+        grafoNaoEuleriano.addVertice(3);
+        grafoNaoEuleriano.addVertice(4);
+        grafoNaoEuleriano.addVertice(5);
+        grafoNaoEuleriano.addVertice(6);
+        grafoNaoEuleriano.addVertice(7);
+        grafoNaoEuleriano.addVertice(8);
+
+        grafoNaoEuleriano.addAresta(1,2);
+        grafoNaoEuleriano.addAresta(2,3);
+        grafoNaoEuleriano.addAresta(2,4);
+        grafoNaoEuleriano.addAresta(3,4);
+        grafoNaoEuleriano.addAresta(3,5);
+        grafoNaoEuleriano.addAresta(4,5);
+        grafoNaoEuleriano.addAresta(5,6);
+        grafoNaoEuleriano.addAresta(6,7);
+        grafoNaoEuleriano.addAresta(6,8);
+        grafoNaoEuleriano.addAresta(7,8);
+
+        
+
+        main.algoritmoLinks(grafoNaoEuleriano);
+
+        main.caminhoCasoNaoEuleriano(grafoNaoEuleriano);
+
+    }
+
     public static void main(String[] args) throws ClassNotFoundException {
         Scanner scan = new Scanner(System.in);
         System.out.println("Olá, qual teste você deseja realizar?");
@@ -208,7 +241,9 @@ public class Main {
         System.out.println("2 - Grafo Conexo de Grau Impar");
         System.out.println("3 - Grafo conexo de Grau par");
         System.out.println("4 - Grafo não Euleriano");      
-        System.out.println("5 - Grafo Aleatório");          
+        System.out.println("5 - Grafo Aleatório");    
+        
+        Teste();
 
         int test = scan.nextInt();
         while (test != 0) {
